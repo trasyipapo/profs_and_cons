1ec41ffbf44a7220d0e7352a52de67a0a51caf56
Trasy Ipapo <trassandra.ipapo@obf.ateneo.edu>
2022-03-14 16:47:33 +0800

    Added background image

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Profs and Cons'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [welcomeUser, loginButton],
-        ),
-      ),
-    );
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            centerTitle: false,
+            backgroundColor: Colors.transparent,
+            title: const Text(
+              'Profs and Cons',
+              textAlign: TextAlign.left,
+            ),
+          ),
+          body: Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/bg-image.png"),
+                    fit: BoxFit.cover)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [welcomeUser, loginButton],
+            ),
+          ),
+        ));
   }
 }
 
@@ -33,11 +45,11 @@ Widget welcomeUser = Container(
           children: const [
             Text('Welcome Atenean!',
                 style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32,
-                )),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Colors.white)),
             Text('Are you ready to start the sem?',
-                style: TextStyle(fontSize: 16))
+                style: TextStyle(fontSize: 16, color: Colors.white))
           ],
         )));
 
