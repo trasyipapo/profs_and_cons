67b9c87913d03e2d146eed3179d8da95b3f31d33
elmodiv <81211833+elmodiv@users.noreply.github.com>
2022-03-15 23:08:16 +0800

    add hardcoded welcome on home page
    search bar not yet centered

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,26 +12,44 @@ class Home extends StatelessWidget {
                 image: DecorationImage(
                     image: AssetImage("assets/bg-image.png"),
                     fit: BoxFit.cover)),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      // onChanged: (value) {
-                      //  filterSearchResults(value);
-                      // },
-                      // controller: editingController,
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          hintText: "Search",
-                          prefixIcon: Icon(Icons.search),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(25.0)))),
-                    ),
-                  ),
-                ])));
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              helloUser,
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: TextField(
+                  // onChanged: (value) {
+                  //  filterSearchResults(value);
+                  // },
+                  // controller: editingController,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(25.0)))),
+                ),
+              ),
+            ])));
   }
 }
+
+Widget helloUser = Container(
+    padding: const EdgeInsets.all(32),
+    child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text('''Hello, 
+Name!''',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Colors.white)),
+            Text("Let's search for your profs",
+                style: TextStyle(fontSize: 16, color: Colors.white))
+          ],
+        )));
