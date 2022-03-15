7ffda71d0037cc54f09efb59e74b2610f6c9de8f
charlize <charlizedelrosario@gmail.com>
2022-03-16 00:30:35 +0800

    created fullreview page file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:profs_and_cons/pages/login.dart';
 import 'package:profs_and_cons/pages/home.dart';
+import 'package:profs_and_cons/pages/fullreview.dart';
 
 void main() {
   runApp(MaterialApp(initialRoute: '/login', routes: {
     '/login': (context) => const Login(),
     '/home': (context) => const Home(),
+    '/fullreview': (context) => const FullReview(),
   }));
 }
---
+++ lib/pages/fullreview.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FullReview extends StatelessWidget {
+  const FullReview({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFFF8F9FF),
+      body: Container(
+        //padding: EdgeInsets.symmetric(),
+        child: Text("hello"),
+      ),
+    );
+  }
+}
