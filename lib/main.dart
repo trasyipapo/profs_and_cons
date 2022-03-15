ce2cab7e18a5e908753b2d8d1f8a0d1a6faf9ab8
charlize <charlizedelrosario@gmail.com>
2022-03-16 02:24:07 +0800

    made GoogleSans the default font

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:profs_and_cons/pages/login.dart';
 import 'package:profs_and_cons/pages/home.dart';
+import 'package:profs_and_cons/pages/profile.dart';
 import 'package:profs_and_cons/pages/fullreview.dart';
 
 void main() {
-  runApp(MaterialApp(initialRoute: '/login', routes: {
-    '/login': (context) => const Login(),
-    '/home': (context) => const Home(),
-    '/fullreview': (context) => const FullReview(),
-  }));
+  runApp(MaterialApp(
+      theme: ThemeData(fontFamily: 'GoogleSans'),
+      initialRoute: '/fullreview',
+      routes: {
+        '/login': (context) => const Login(),
+        '/home': (context) => const Home(),
+        '/profile': (context) => const Profile(),
+        '/fullreview': (context) => const FullReview(),
+      }));
 }
---
+++ lib/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const TextStyle header =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 28, color: Colors.black);
+
+const TextStyle header2 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black);
+
+const TextStyle overallRating =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 28, color: Colors.blue);
+
+const TextStyle bodyText =
+    TextStyle(fontWeight: FontWeight.normal, fontSize: 12, color: Colors.black);
+
+const TextStyle buttonText =
+    TextStyle(fontWeight: FontWeight.normal, fontSize: 12, color: Colors.white);
+
+const TextStyle smallText =
+    TextStyle(fontWeight: FontWeight.normal, fontSize: 9, color: Colors.grey);
