2f5b055bd292a5fcbcdbe1926d0d7ed892541daa
elmodiv <81211833+elmodiv@users.noreply.github.com>
2022-03-16 14:48:53 +0800

    add revlist page

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:profs_and_cons/pages/login.dart';
 import 'package:profs_and_cons/pages/home.dart';
 import 'package:profs_and_cons/pages/profile.dart';
+import 'package:profs_and_cons/pages/revlist.dart';
 
 void main() {
-  runApp(MaterialApp(initialRoute: '/profile', routes: {
+  runApp(MaterialApp(initialRoute: '/revlist', routes: {
     '/login': (context) => const Login(),
     '/home': (context) => const Home(),
     '/profile': (context) => const Profile(),
+    '/revlist': (context) => const RevList(),
   }));
 }
