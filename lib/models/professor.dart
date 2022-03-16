4ed0b3951314f5d42c52e4a3ab97db97ee5d0f0e
Jacob Simon G. Bernardo <jacobsimonbernardo@gmail.com>
2022-03-17 00:43:44 +0800

    added department subtitles

--- lib/models/professor.dart
+++ lib/models/professor.dart
@@ -1,27 +1,16 @@
-// class Professor {
-//   int professorId;
-//   String firstName;
-
-//   Professor.fromJson(Map json)
-//       : professorId = int.parse(json['professor_id']),
-//         firstName = json['first_name'];
-
-//   Map toJson() {
-//     return {'professor_id': professorId, 'first_name': firstName};
-//   }
-// }
-
 class Professor {
   int id;
   String firstName;
   String lastName;
   double overallRating;
+  String department;
 
   Professor(
       {required this.id,
       required this.firstName,
       required this.lastName,
-      required this.overallRating});
+      required this.overallRating,
+      required this.department});
 
   factory Professor.fromJson(Map<String, dynamic> json) {
     return Professor(
@@ -29,6 +18,7 @@ class Professor {
       firstName: json['first_name'] as String,
       lastName: json['last_name'] as String,
       overallRating: double.parse(json['overall_ave']),
+      department: json['department'] as String,
     );
   }
 }
